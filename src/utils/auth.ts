b102f6108f753c1eb857a5b00c4d59ed764d1c4f
Ariiq354 <realgoldcraft@gmail.com>
2026-09-24 14:55:29 +0700

    fix: cors

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -62,6 +62,7 @@ export const auth = betterAuth({
     defaultCookieAttributes: {
       sameSite: 'none',
       secure: true,
+      partitioned: true,
     },
   },
   plugins: [openAPI(), username(), adminPlugins()],
--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,7 +1,11 @@
 import { cors } from '@elysia/cors'
 
 export const CorsPlugin = cors({
-  origin: ['http://localhost:3000', 'https://ubberkahamanah.my.id'],
+  origin: [
+    'http://localhost:3000',
+    'https://ubberkahamanah.my.id',
+    'https://ba-fe-production.up.railway.app',
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
